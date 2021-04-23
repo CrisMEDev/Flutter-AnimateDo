cd32b14686ad13d75f2485e34ea1547970ff89a3
Cristian Morales <cr15moresp@gmail.com>
2021-04-23 17:56:38 -0500

    Inicio del proyecto

---
+++ lib/src/pages/pagina1_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Hola mundo'),
+        ),
+      ),
+    );
+  }
+}
